bc8bc32305bba37bd14c0d87312d061e1a76741e
Stefan Badelt <badelt@caltech.edu>
2018-05-14 17:10:20 -0700

    repaired setup.py depencencies

--- peppercornenumerator/__init__.py
+++ peppercornenumerator/__init__.py
@@ -2,7 +2,7 @@
 #  __init__.py
 #  EnumeratorProject
 #
-__version__ = "v0.6"
+__version__ = "v0.6.1"
 
 from peppercornenumerator.enumerator import Enumerator, PolymerizationError
 from peppercornenumerator.condense import ReactionGraph, CondensationError, PepperCondensation
--- setup.py
+++ setup.py
@@ -1,31 +1,33 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
 
-with open('README.md') as f:
-    readme = f.read()
-
-with open('LICENSE') as f:
-    license = f.read()
+LONG_DESCRIPTION="""
+This package enumerates domain-level strand displacement (DSD) reaction
+networks assuming low species concentrations, such that unimolecular reaction
+pathways always equilibrate before bimolecular reactions initiate. The
+enumerator can handle arbitrary non-pseudoknotted structures and supports a
+diverse set of unimolecular and bimolecular domain-level reactions: bind/unbind
+reactions, 3-way branch-migration and 4-way branch-migration reactions and
+remote toehold migration. For more background on reaction semantics we refer to
+the publication [Grun et al. (2014)].
+"""
 
 setup(
     name='peppercornenumerator',
-    version='0.6',
+    version='0.6.1',
     description='Domain-level nucleic acid reaction enumerator',
-    long_description=readme,
+    long_description=LONG_DESCRIPTION,
     author='Casey Grun, Stefan Badelt, Karthik Sarma, Brian Wolfe, Seung Woo Shin and Erik Winfree',
     author_email='winfree@caltech.edu',
-    license=license,
+    license='MIT',
     test_suite='tests',
     install_requires=[
         'numpy',
         'pyparsing>=1.5.5', 
         'crnsimulator==0.4',
-        'dsdobjects==0.6'],
-    dependency_links=[
-        'https://github.com/bad-ants-fleet/crnsimulator/tarball/master#egg=crnsimulator-0.4',
-        'http://github.com/DNA-and-Natural-Algorithms-Group/dsdobjects/tarball/master#egg=dsdobjects-0.6'],
+        'dsdobjects==0.6.1'],
+    dependency_links=['https://github.com/bad-ants-fleet/crnsimulator/archive/v0.4.tar.gz#egg=crnsimulator-0.4'],
     packages=['peppercornenumerator'],
     scripts=['scripts/peppercorn',
              'scripts/pilsimulator']
