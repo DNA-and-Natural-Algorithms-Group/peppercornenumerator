699e386a0e49e2eaff54ea92afca3eb89d9f0c7b
Stefan Badelt <stefan.badelt@gmail.com>
2020-03-07 23:37:36 -0800

    notebook cleanup and runtime tests

--- case_studies/kotani2017.py
+++ case_studies/kotani2017.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 
 def kotani2017_F2_pil(x):
     return """
@@ -152,8 +153,7 @@ def data(evaluate=False, verbose = 0):
     from figure_analysis import FigureData
 
     # Default pilsimulator call
-    # If you use --no-jacobian, #7 doesn't show the 0 nM trajectory
-    psim = "pilsimulator --nxy --header --atol 1e-12 --rtol 1e-12 --mxstep 1000"
+    psim = "pilsimulator --no-jacobian --nxy --header --atol 1e-12 --rtol 1e-12 --mxstep 1000"
     h10l = " --t0 0 --t8 36000 --t-lin 18000"
     h20l = " --t0 0 --t8 72000 --t-lin 18000"
     h30log = " --t0 0.1 --t8 108000 --t-log 18000"
@@ -185,8 +185,6 @@ def data(evaluate=False, verbose = 0):
         reporter = 'D'
         metric = 'completion-time'
         current.add_system_simulation_setup(pilstring, simulation, reporter, metric, '5', res, simargs=sim[sim.find('C1='):])
-
-
 
     current.pepperargs['default'] = current.pepperargs['condensed'].copy()
     current.pepperargs['default']['release_cutoff'] = 7
@@ -291,5 +289,8 @@ def data(evaluate=False, verbose = 0):
     return [k17_F2, k17_F3, k17_F4]
 
 if __name__ == '__main__':
+    print(kotani2017_F2_pil(None))
+    print(kotani2017_F3_pil(None))
+    print(kotani2017_F4_pil(None))
     data(evaluate=True, verbose=1)
 
