315a9e65c6a2810e25cad41dc521023a274d9bcb
Stefan Badelt <bad-ants-fleet@posteo.eu>
2020-06-11 10:35:04 +0200

    set development status to stable

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     author_email='winfree@caltech.edu',
     license='MIT',
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         ],
